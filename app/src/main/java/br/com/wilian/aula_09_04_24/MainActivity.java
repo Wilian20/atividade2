f2c4555d5693f5ef67376cf53cbd85dadd67c5ad
wilian <demarquecontardi@gmail.com>
2024-04-16 19:07:05 -0700

    atividade_avaliativa2

---
+++ app/src/main/java/br/com/wilian/aula_09_04_24/MainActivity.java
@@ -0,0 +1,35 @@
+package br.com.wilian.aula_09_04_24;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    startActivity(new Intent(MainActivity.this, ActivityLogin.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+
+                }
+            }
+
+        };
+        thread1.start();
+    }
+}
